5fa96aa7b46615f1e72cf09bd22f513ea2b9bb34
nishant <nishantchopra31@gmail.com>
2015-06-17 13:58:21 +0530

    hy

--- blog/views.py
+++ blog/views.py
@@ -14,9 +14,11 @@ def post_list(request):
 def post_list1(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list1.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
